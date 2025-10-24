080773e322bab1eae227b3a0189db9c9e2cfdff4
Sajid-8484 <f2025-1016@bnu.edu.pk>
2025-10-24 10:29:18 +0500

    Create Checkboard stars pattern.cpp
    Checkboard Stars Pattern

---
+++ Checkboard stars pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter a Number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){ 
+        if (i % 2 == 0)
+            cout << " ";
+        for(int j=1;j<=n;j++){
+            cout<<"*";
+        }
+         cout << endl;
+    }
+    return 0;
+}
